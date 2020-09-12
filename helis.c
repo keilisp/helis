d5dbebfa1b63ea4a6fd34f4c96976c3369166aec
mediocre <druksasha@ukr.net>
2020-09-12 23:42:06 +0300

    handle tabs

--- helis.c
+++ helis.c
@@ -17,6 +17,7 @@
 /* Defines */
 #define CTRL_KEY(k) ((k)&0x1f)
 #define HELIS_VERSION "0.0.0.0.1"
+#define HELIS_TAB_STOP 4
 
 // Keys bindings
 enum editorKey {
@@ -33,14 +34,17 @@ enum editorKey {
 
 // Row of text
 typedef struct erow {
-  int size;
+  int size;  // Chars size
+  int rsize; // Render size
   char *chars;
+  char *render;
 } erow;
 
 /* Data */
 // Editor config
 struct editorConfig {
   int cx, cy;     // Cursor coords
+  int rx;         // Render coord
   int rowoff;     // Row offset
   int coloff;     // Column offset
   int screenrows; // Screen row count
@@ -55,6 +59,48 @@ struct editorConfig E;
 
 /* Row */
 
+// Find out rx
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t') {
+      rx += (HELIS_TAB_STOP - 1) - (rx % HELIS_TAB_STOP);
+    }
+    rx++;
+  }
+  return rx;
+}
+
+// Update Row
+void editorUpdateRow(erow *row) {
+
+  // Handling tabs
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t')
+      tabs++;
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + tabs * (HELIS_TAB_STOP - 1) + 1);
+
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while ((idx % HELIS_TAB_STOP) != 0)
+        row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 // Append row
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
@@ -64,6 +110,11 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -280,17 +331,22 @@ void abFree(struct abuf *ab) { free(ab->b); }
 
 // Scrolling
 void editorScroll() {
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
+
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff) {
-    E.coloff = E.cx;
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols) {
-    E.coloff = E.cx - E.screencols + 1;
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 // Drawing rows
@@ -321,12 +377,12 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, ">", 1);
       }
     } else {
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
       if (len < 0)
         len = 0;
       if (len > E.screencols)
         len = E.screencols;
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
     // Clear the line when redrawing
     abAppend(ab, "\x1b[K", 3);
@@ -348,10 +404,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  // Move cursor to E.cx and E.cy
+  // Move cursor to E.rx and E.cy
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-           (E.cx - E.coloff) + 1);
+           (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   // Showing cursor
@@ -445,6 +501,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.numrows = 0;
   E.coloff = 0;
